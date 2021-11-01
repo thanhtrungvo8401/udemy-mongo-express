f52f02d097c6c412d0e798da9aef87dcf874e584
trungvt <thanhtrungvo8401@gmail.com>
2021-11-01 20:09:49 +0700

    master-21: install color for console.log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 
 
@@ -28,11 +29,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 // SERVER RUN:
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections:
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     // Close server and exit process:
     server.close(() => process.exit(1))
 });
