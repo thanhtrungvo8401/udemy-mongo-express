6da73ba8a77989cb8fbd038956742d2f908b040a
trungvt <thanhtrungvo8401@gmail.com>
2021-11-01 19:58:35 +0700

    master-20: connect with mongoDB and catch unhandledRejection

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+
 
 // Load env vars:
 dotenv.config({
     path: "./config/config.env"
 })
+
+// Connect to DB:
+connectDB();
 
 // Route files:
 const bootcamps = require("./routes/bootcamps");
@@ -21,6 +26,13 @@ if (process.env.NODE_ENV === NODE_ENV.DEV) {
 // Mount routes:
 app.use('/api/v1/bootcamps', bootcamps);
 
+// SERVER RUN:
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Handle unhandled promise rejections:
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server and exit process:
+    server.close(() => process.exit(1))
+});
