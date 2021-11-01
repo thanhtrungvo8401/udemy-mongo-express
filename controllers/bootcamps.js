1e4d912c78d74e44dbbe5fefec29d1f2fa8ef683
trungvt <thanhtrungvo8401@gmail.com>
2021-11-02 05:45:59 +0700

    master-23: handle fetch bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,32 @@ const Bootcamp = require("../models/Bootcamp");
 // @Desc Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false });
+  }
 }
 
 // @Desc Get a bootcamp
 // @route GET /api/v1/bootcamps/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req?.params?.id)
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false, data: {} })
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false });
+  }
 }
 
 // @Desc Create a new bootcamp
