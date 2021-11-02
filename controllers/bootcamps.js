c778623c83156d90d2891241ffaed21bcd35f4e3
trungvt <thanhtrungvo8401@gmail.com>
2021-11-02 15:49:18 +0700

    master-35: select and sort

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,7 +14,7 @@ exports.getBootcamps = asyncHanlder(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // fields to exclude:
-  const removeFields = ["select"];
+  const removeFields = ["select", "sort"];
   // Loop over removeFields and delete them from reqQuery:
   removeFields.forEach(param => delete reqQuery[param])
 
@@ -25,11 +25,19 @@ exports.getBootcamps = asyncHanlder(async (req, res, next) => {
 
   // Finding resource:
   let query = Bootcamp.find(JSON.parse(queryStr));
-  
+
   // Select fields:
   if (req.query.select) {
     const fields = req.query.select.split(",").join(" ");
     query = query.select(fields);
+  }
+
+  // Sort:
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort("-createdAt")
   }
 
   const bootcamps = await query;
