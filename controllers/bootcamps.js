8ef42e07a4ed173d762411c05fd0e0d7f951ac86
trungvt <thanhtrungvo8401@gmail.com>
2021-11-02 15:50:54 +0700

    master-35: select and sort

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,29 +10,24 @@ const { query } = require("express");
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHanlder(async (req, res, next) => {
-  // coppy req.query:
   const reqQuery = { ...req.query };
 
   // fields to exclude:
   const removeFields = ["select", "sort"];
-  // Loop over removeFields and delete them from reqQuery:
   removeFields.forEach(param => delete reqQuery[param])
 
-  // create query string:
-  let queryStr = JSON.stringify(req.query);
   // create operators ($gt, $gte, etc..)
+  let queryStr = JSON.stringify(req.query);
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-  // Finding resource:
+  // FETCH DATA:
   let query = Bootcamp.find(JSON.parse(queryStr));
 
-  // Select fields:
+  // SELECT, SORT:
   if (req.query.select) {
     const fields = req.query.select.split(",").join(" ");
     query = query.select(fields);
   }
-
-  // Sort:
   if (req.query.sort) {
     const sortBy = req.query.sort.split(",").join(" ");
     query = query.sort(sortBy)
