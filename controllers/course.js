7669bfd808f9e4bf5581baadfff91838166d27a4
trungvt <thanhtrungvo8401@gmail.com>
2021-11-02 22:06:36 +0700

    master-21: create single course

--- controllers/course.js
+++ controllers/course.js
@@ -1,4 +1,5 @@
 const Course = require("../models/Course");
+const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const { STATUS_CODE } = require("../constant");
@@ -27,5 +28,43 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses,
+  })
+})
+
+// @Desc Get a single course
+// @route GET /api/v1/courses/:id
+// @access Public
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description"
+  })
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  })
+})
+
+// @Desc Create a single course
+// @route POST /api/v1/bootcamps/:bootcampId/courses
+// @access Private
+
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  const bootcamp = Bootcamp.findById(req.params.bootcampId);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
+  }
+
+  req.body.bootcamp = req.params.bootcampId;
+  const course = await Course.create(req.body)
+  res.status(200).json({
+    success: true,
+    data: course,
   })
 })
--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getCourses } = require("../controllers/course");
+const { getCourses, getCourse, addCourse } = require("../controllers/course");
 
-router.route("/").get(getCourses);
+router.route("/").get(getCourses).post(addCourse);
+router.route("/:id").get(getCourse);
 
 module.exports = router;
