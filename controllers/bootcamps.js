8038c77612922e3a4958fcb362ec20f30310a2b7
trungvt <thanhtrungvo8401@gmail.com>
2021-11-02 14:42:28 +0700

    master-34: Advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,10 @@ const asyncHanlder = require("../middleware/async");
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHanlder(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let queryStr = JSON.stringify(req.query);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+
+  const bootcamps = await Bootcamp.find(JSON.parse(queryStr));
   res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
 
